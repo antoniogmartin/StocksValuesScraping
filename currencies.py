16a9e8a86990b95265f268c529bcd4b23fa4f86e
Antonio Guzmán <antonio.gm011@gmail.com>
2018-11-05 20:08:17 +0100

    currencies updated

--- currencies.py
+++ currencies.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[112]:
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -13,18 +7,23 @@ if __name__ == '__main__':
     page = requests.get(str)
     soup = BeautifulSoup(page.content)
     c_name=soup.findAll("li", {"class": "titCurren01"})
-    c_val = soup.findAll("li", {"class": "txtCurren_ult equal"})
+    c_val = soup.findAll("li", {"class": "txtCurren_ult"})
     c_date= soup.find("span",{"class": "dataCurren01"})
-    print(c_date.text)
     i=0
     c_res=[]
-    while(i<len(c_name)):
-        c_pair=c_name[i].text.replace('\n','').split('/')
-        c_res.append((c_pair[0],c_pair[1],c_val[i].text,c_date.text ))
-        i+=1
-    print(c_res)
+    if (len(c_val)>=len(c_name)):
+        while(i<len(c_name)):
+            c_pair=c_name[i].text.replace('\n','').split('/')
+            
+            c_res.append((c_pair[0],c_pair[1],c_val[i].text,c_date.text ))
+            i+=1
+    else:
+        while(i<len(c_val)):
+            c_pair=c_name[i].text.replace('\n','').split('/')
+            
+            c_res.append((c_pair[0],c_pair[1],c_val[i].text,c_date.text ))
+            i+=1
     labels = ['CURRENCY_1','CURRENCY_2', 'VALUE','DATE']
 
-
-pd.DataFrame.from_records(c_res, columns=labels)
-
+df=pd.DataFrame.from_records(c_res, columns=labels)
+print(df)
