ab5cd22d60511c817a3abf9c17ed983671584993
Joaquín Fernández <joferle95@gmail.com>
2018-11-05 20:02:10 +0100

    Update StocksWebScraping.py

--- StocksWebScraping.py
+++ StocksWebScraping.py
@@ -1,68 +1,86 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import requests
 import lxml.html as lh
 import pandas as pd
+import bs4
+from bs4 import BeautifulSoup
+import datetime
 
-url='http://www.infobolsa.es/acciones/ibex35'
-#Create a handle, page, to handle the contents of the website
-page = requests.get(url)
-#Store the contents of the website under doc
-doc = lh.fromstring(page.content)
-#Parse data that are stored between <tr>..</tr> of HTML
-tr_elements = doc.xpath('//tr')
-
-#Check the length of the first 12 rows
-[len(T) for T in tr_elements[:15]]
-tr_elements = doc.xpath('//tr')
-#Create empty list
-col=[]
-i=0
-#For each row, store each first element (header) and an empty list
-for t in tr_elements[0]:
-    i+=1
-    name=t.text_content()
-    f'{i}:"{name}"'
-    col.append((name,[]))
-
-#Since out first row is the header, data is stored on the second row onwards
-for j in range(1,len(tr_elements)):
-    #T is our j'th row
-    T=tr_elements[j]
+def title_of_table( page ):
+    page_content = BeautifulSoup(page.content)
+    elem = page_content.find("a", attrs={"class":"textOverflow select"})
+    text = elem.text
+    text = text.replace('\n','') 
+    text = text.replace('\r','') 
+    text = text.replace(' ','')
     
-    #If row is not of size 10, the //tr data is not from our table 
-    if len(T)!=15:
-        break
-    
-    #i is the index of our column
+    return text;
+def writeInCSV( df, title ):
+    now = datetime.datetime.now()
+    current_date =now.strftime("%Y-%m-%d %H_%M")
+    file_name = title + '_' + current_date + '.csv'
+    df.to_csv(file_name)
+
+    return
+
+def get_table( url, size ):
+
+    #Create a handle, page, to handle the contents of the website
+    page = requests.get(url)
+    #Store the contents of the website under doc
+    doc = lh.fromstring(page.content)
+    #Parse data that are stored between <tr>..</tr> of HTML
+    tr_elements = doc.xpath('//tr')
+    #Check the length of the first 12 rows
+    [len(T) for T in tr_elements[:size]]
+
+    tr_elements = doc.xpath('//tr')
+    #Create empty list
+    col=[]
     i=0
-    
-    #Iterate through each element of the row
-    for t in T.iterchildren():
-        data=t.text_content() 
-        #Check if row is empty
-        if i>0:
-        #Convert any numerical value to integers
-            try:
-                data=int(data)
-            except:
-                pass
-        #Append the data to the empty list of the i'th column
-        col[i][1].append(data)
-        #Increment i for the next column
+    #For each row, store each first element (header) and an empty list
+    for t in tr_elements[0]:
         i+=1
+        name=t.text_content()
+        f'{i}:"{name}"'
+        col.append((name,[]))
+    #Since out first row is the header, data is stored on the second row onwards
+    for j in range(1,len(tr_elements)):
+        #T is our j'th row
+        T=tr_elements[j]
 
-[len(C) for (title,C) in col]
+        #If row is not of size 10, the //tr data is not from our table 
+        if len(T)!=size:
+            break
 
-Dict={title:column for (title,column) in col}
-df=pd.DataFrame(Dict)
-"\n\r".rstrip("\n\r")
+        #i is the index of our column
+        i=0
 
-df = df.replace('\n',' ', regex=True) 
-df = df.replace('\r',' ', regex=True) 
-df
-       
+        #Iterate through each element of the row
+        for t in T.iterchildren():
+            data=t.text_content() 
 
+            #Append the data to the empty list of the i'th column
+            col[i][1].append(data)
+            #Increment i for the next column
+            i+=1
 
+            [len(C) for (title,C) in col]
 
+    Dict={title:column for (title,column) in col}
+    df=pd.DataFrame(Dict)
+    #"\n\r".rstrip("\n\r")
+
+    df = df.replace('\n',' ', regex=True) 
+    df = df.replace('\r',' ', regex=True) 
+    
+    return df;
+
+#We store all pages and their sizes
+url = ['http://www.infobolsa.es/acciones/ibex35', 'http://www.infobolsa.es/acciones/nasdaq' ]
+size = [15,11]
+
+for i in range(len(url)):    
+    #to get the page
+    page = requests.get(url[i])
+    title = title_of_table( page )
+    writeInCSV( get_table(url[i], size[i]), title )
